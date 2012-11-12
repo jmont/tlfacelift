8c5add60714fc7e1f62a6328d63e1c82ffc59ca6
Juan Carlos Montemayor Elosua <j.mont@me.com>
2012-11-12 18:54:51 -0500

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+function init() {
+    //fill columns using content here
+    $("#currentColumn").html("Current");
+    $("#listingsColumn").html("Listing");
+    $("#eventsColumn").html("Event");
+}
+
+window.addEventListener("load", init, false);

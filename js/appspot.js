8156c7be7fd62d3b642a9a61777a2b6c2ad0cbcd
fredteumer <frederick.teumer@tufts.edu>
2012-12-02 20:58:33 -0500

    redid grid in JS

--- js/appspot.js
+++ js/appspot.js
@@ -1,28 +1,4 @@
 function init() {
-    //fill columns using content here
-    var tickets = "<a class=\"categoryButton\">Tickets</a>"
-    var music = "<a class=\"categoryButton\">Music</a>"
-    var events = "<div class=\"contentBox\" id=\"events\">Events</div>"
-    var transportaion = "<div class=\"contentBox\" id=\"transportation\">Transportation</div>"
-    $("#topLevel").append(tickets);
-    $("#topLevel").append(music);
-    
-    var title = "<div class=\"title bold\">LISTINGS</div>"
-    var campus = "<div class=\"contentBox\" id=\"campus\">Campus</div>"
-    var sublets = "<div class=\"contentBox\" id=\"sublets\">Sublets</div>"
-    var jobs = "<div class=\"contentBox\" id=\"jobs\">Jobs</div>"
-    $("#listingsColumn").append(title);
-    $("#listingsColumn").append(campus);
-    $("#listingsColumn").append(sublets);
-    $("#listingsColumn").append(jobs);
-    
-    title = "<div class=\"title bold\">EVENTS</div>"
-    var today = "<div class=\"contentBox\" id=\"today\">Today</div>"
-    var tomorrow = "<div class=\"contentBox\" id=\"tomorrow\">Tomorrow</div>"
-    $("#eventsColumn").append(title);
-    $("#eventsColumn").append(today);
-    $("#eventsColumn").append(tomorrow);
-    
     evenOutChildren("#currentColumn");
     
     var navBar = $("#tlnav").clone();
@@ -30,6 +6,18 @@ function init() {
     var curClass = navBar.attr("class");
     navBar.attr("class",curClass + " visible-phone column");
     $("#content").append(navBar);
+    
+    //stuff to fill grid
+    var appfill = "<div class=\"app-unit\">\n\
+    <img width=\"75\" height=\"75\"src=\"img/filler.gif\" alt=\"filler.gif\" />\
+    \n<h6>Oops...trololol!</h6>\n</div>";
+    
+    for(var j=0; j<=1; j++){
+    	for(var i=0; i<=2; i++){
+    		$("#g-"+i+"-"+j).append(appfill);
+    	}
+    }
+    
     
 }
 

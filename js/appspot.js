e83035c8035a17cbac509619542faa3b81612b97
fredteumer <frederick.teumer@tufts.edu>
2012-12-03 22:40:12 -0500

    committing working grid

--- js/appspot.js
+++ js/appspot.js
@@ -7,12 +7,19 @@ function init() {
     navBar.attr("class",curClass + " visible-phone column");
     $("#content").append(navBar);
     
+    //sidebar
+    var sidebar = "<ul id=\"sidebar-elems\"> \
+					<li class=\"categoryButton\">Most Popular</li> \
+					<li class=\"categoryButton\">Recently Added</li> \
+				   </ul>";
+	$("#sidebar").append(sidebar);
+    
     //stuff to fill grid
     var appfill = "<div class=\"app-unit\">\n\
     <img width=\"75\" height=\"75\"src=\"img/filler.gif\" alt=\"filler.gif\" />\
     \n<h6>Oops...trololol!</h6>\n</div>";
     
-    for(var j=0; j<=1; j++){
+    for(var j=0; j<=2; j++){
     	for(var i=0; i<=2; i++){
     		$("#g-"+i+"-"+j).append(appfill);
     	}

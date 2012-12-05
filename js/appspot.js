45fccf3583ce8aff11be439baddd59d044bf9a5f
fredteumer <frederick.teumer@tufts.edu>
2012-12-05 12:38:07 -0500

    fixed grid some. working on sidebar

--- js/appspot.js
+++ js/appspot.js
@@ -1,23 +1,13 @@
 function init() {
-    evenOutChildren("#currentColumn");
-    
-    var navBar = $("#tlnav").clone();
-    navBar.attr("id","tlnavBottom");
-    var curClass = navBar.attr("class");
-    navBar.attr("class",curClass + " visible-phone column");
-    $("#content").append(navBar);
     
     //sidebar
-    var sidebar = "<ul id=\"sidebar-elems\"> \
-					<li class=\"categoryButton\">Most Popular</li> \
-					<li class=\"categoryButton\">Recently Added</li> \
-				   </ul>";
-	$("#sidebar").append(sidebar);
-    
+    var side_elems = ["All Apps", "Most Popular", "Recently Added"];
+    displayList("#sidebar", side_elems);
+
     //stuff to fill grid
     var appfill = "<div class=\"app-unit\">\n\
-    <img width=\"75\" height=\"75\"src=\"img/filler.gif\" alt=\"filler.gif\" />\
-    \n<h6>Oops...trololol!</h6>\n</div>";
+    <img width=\"75\" height=\"75\"src=\"img/filler.gif\" \
+    alt=\"filler.gif\" />\n<h6>Oops...trololol!</h6>\n</div>";
     
     for(var j=0; j<=2; j++){
     	for(var i=0; i<=2; i++){
@@ -25,9 +15,30 @@ function init() {
     	}
     }
     
+    //existing code for navbar
+    //aka dont touch
+    evenOutChildren("#currentColumn");
     
+    var navBar = $("#tlnav").clone();
+    navBar.attr("id","tlnavBottom");
+    var curClass = navBar.attr("class");
+    navBar.attr("class",curClass + " visible-phone column");
+    $("#content").append(navBar);
 }
 
+//my code
+
+function displayList(id, aList) {
+	$(id).html("");
+	for (i in aList){
+			var it = "<a class=\"categoryButton\" onclick=buttonClicked(this)>" + aList[i] + "</a>";
+   			$(id).append(it);
+	}
+}
+
+
+//existing js code
+//dont touch
 function evenOutChildren(divID) {
     var hei = 0;
     $(divID).children().each(function (x) {
